22b515710204277816e8f8670ad4547bb9f2808a
Sharvari Joshi <sharvarijoshi@Sharvaris-MacBook-Air.local>
2025-01-29 18:59:07 -0700

    Project Initialization

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+
+import { Card, FormField, Loader } from '../components';
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
